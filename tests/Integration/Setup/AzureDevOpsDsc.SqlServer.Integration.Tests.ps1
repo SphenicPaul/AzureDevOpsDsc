efd8d77d9dda1b3f9f667a91053e36532d438cc0
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2021-01-29 20:58:05 +0000

    Added initial 'SqlServer', integration test setup scripts.

---
+++ tests/Integration/Setup/AzureDevOpsDsc.SqlServer.Integration.Tests.ps1
@@ -0,0 +1,119 @@
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '\..\..\Unit\Modules\TestHelpers\CommonTestHelper.psm1')
+
+if (-not (Test-BuildCategory -Type 'Integration'))
+{
+    return
+}
+
+$script:dscModuleName = 'AzureDevOpsDsc'
+
+$script:azureDevOpsDscCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\..\Source\Modules\AzureDevOpsDsc.Common'
+Import-Module -Name $script:azureDevOpsDscCommonModulePath
+
+try
+{
+    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
+}
+catch [System.IO.FileNotFoundException]
+{
+    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
+}
+
+$script:testEnvironment = Initialize-TestEnvironment `
+    -DSCModuleName $script:dscModuleName `
+    -DSCResourceName $script:dscResourceName `
+    -ResourceType 'Mof' `
+    -TestType 'Integration'
+
+
+<#
+    .SYNOPSIS
+        This function will output the Setup Bootstrap Summary.txt log file.
+    .DESCRIPTION
+        This function will output the Summary.txt log file, this is to be
+        able to debug any problems that potentially occurred during setup.
+        This will pick up the newest Summary.txt log file, so any
+        other log files will be ignored (AppVeyor build worker has
+        SQL Server instances installed by default).
+        This code is meant to work regardless what SQL Server
+        major version is used for the integration test.
+#>
+function Show-SqlBootstrapLog
+{
+    [CmdletBinding()]
+    param
+    (
+    )
+
+    $summaryLogPath = Get-ChildItem -Path 'C:\Program Files\Microsoft SQL Server\**\Setup Bootstrap\Log\Summary.txt' |
+        Sort-Object -Property LastWriteTime -Descending |
+        Select-Object -First 1
+
+    $summaryLog = Get-Content $summaryLogPath
+
+    Write-Verbose -Message $('-' * 80) -Verbose
+    Write-Verbose -Message 'Summary.txt' -Verbose
+    Write-Verbose -Message $('-' * 80) -Verbose
+
+    $summaryLog | ForEach-Object -Process {
+        Write-Verbose $_ -Verbose
+    }
+
+    Write-Verbose -Message $('-' * 80) -Verbose
+}
+
+
+try
+{
+    $configFile = Join-Path -Path $PSScriptRoot -ChildPath "010.AzureDevOpsDsc.SqlServer.config.ps1"
+    . $configFile
+
+
+    # By switching to 'SilentlyContinue' should theoretically increase the download speed.
+    $previousProgressPreference = $ProgressPreference
+    $ProgressPreference = 'SilentlyContinue'
+
+
+    # Download SQL Server media
+    if (-not (Test-Path -Path $ConfigurationData.AllNodes.ImagePath))
+    {
+        # Download the EXE used to download the ISO
+        Write-Verbose -Message "Start downloading the SQL Server media at $(Get-Date -Format 'yyyy-MM-dd hh:mm:ss')..." -Verbose
+        Invoke-WebRequest -Uri $script:mockSourceDownloadExeUrl -OutFile $ConfigurationData.AllNodes.DownloadExePath | Out-Null
+
+        # Download ISO using the EXE
+        $imageDirectoryPath = Split-Path -Path $ConfigurationData.AllNodes.ImagePath -Parent
+        $downloadExeArgumentList = '/ENU /Quiet /HideProgressBar /Action=Download /Language=en-US /MediaType=ISO /MediaPath={0}' -f $imageDirectoryPath
+        Start-Process -FilePath $ConfigurationData.AllNodes.DownloadExePath `
+                    -ArgumentList $downloadExeArgumentList `
+                    -Wait
+
+        # Rename the ISO and generate SHA1 hash
+        Rename-Item -Path $ConfigurationData.AllNodes.DownloadIsoPath `
+                    -NewName $(Split-Path -Path $ConfigurationData.AllNodes.ImagePath -Leaf) | Out-Null
+        Write-Verbose -Message ('SQL Server media file has SHA1 hash ''{0}''.' -f (Get-FileHash -Path $ConfigurationData.AllNodes.ImagePath -Algorithm 'SHA1').Hash) -Verbose
+
+        # Return to previous 'ProgressPreference' value
+        $ProgressPreference = $previousProgressPreference
+
+        # Double check that the SQL media was downloaded.
+        if (-not (Test-Path -Path $ConfigurationData.AllNodes.ImagePath))
+        {
+            Write-Warning -Message ('SQL media could not be downloaded, can not run the integration test.')
+            return
+        }
+        else
+        {
+            Write-Verbose -Message "Finished downloading the SQL Server media iso at $(Get-Date -Format 'yyyy-MM-dd hh:mm:ss')." -Verbose
+        }
+    }
+    else
+    {
+        Write-Verbose -Message 'SQL Server media is already present/downloaded.' -Verbose
+    }
+
+}
+finally
+{
+    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+}
---
+++ tests/Integration/Setup/AzureDevOpsDsc.SqlServer.config.ps1
@@ -0,0 +1,201 @@
+
+
+# Get a spare drive letter
+$mockLastDrive = ((Get-Volume).DriveLetter | Sort-Object | Select-Object -Last 1)
+$mockIsoMediaDriveLetter = [char](([int][char]$mockLastDrive) + 1)
+
+
+# Information relating to the SQL Server instance to be installed
+$sqlServerVersionData = @{
+
+    Version = '150'
+    IsoImageName = 'SQL2019.iso'
+
+    # Additional variables required as ISO is downloaded via additional EXE
+    SourceDownloadExeUrl = 'https://download.microsoft.com/download/d/a/2/da259851-b941-459d-989c-54a18a5d44dd/SQL2019-SSEI-Dev.exe'
+
+    DownloadExeName = 'SQL2019-SSEI-Dev.exe'
+    DownloadIsoName = 'SQLServer2019-x64-ENU-Dev.iso'
+}
+
+
+# Configuration information/data used within the SQL Server installation
+$ConfigurationData = @{
+    AllNodes = @(
+        @{
+            NodeName                                = 'localhost'
+
+            SqlServerInstanceIdPrefix               = 'MSSQL'
+            #AnalysisServiceInstanceIdPrefix         = 'MSAS'
+
+            # Database Engine properties.
+            DatabaseEngineNamedInstanceName         = 'AZDEVOPS'
+            DatabaseEngineNamedInstanceFeatures     = 'SQLENGINE'           # ',REPLICATION,AS,CONN,BC,SDK'
+            #AnalysisServicesMultiServerMode         = 'MULTIDIMENSIONAL'
+            DatabaseEngineDefaultInstanceName       = 'MSSQLSERVER'
+            DatabaseEngineDefaultInstanceFeatures   = 'SQLENGINE'           # ',REPLICATION,CONN,BC,SDK'
+
+            # General SqlSetup properties
+            Collation                               = 'Latin1_General_CI_AS'
+            InstanceDir                             = 'C:\Program Files\Microsoft SQL Server'
+            InstallSharedDir                        = 'C:\Program Files\Microsoft SQL Server'
+            InstallSharedWOWDir                     = 'C:\Program Files (x86)\Microsoft SQL Server'
+            InstallSQLDataDir                       = "C:\Db\$($sqlServerVersionData.Version)\System"
+            SQLUserDBDir                            = "C:\Db\$($sqlServerVersionData.Version)\Data\"
+            SQLUserDBLogDir                         = "C:\Db\$($sqlServerVersionData.Version)\Log\"
+            SQLBackupDir                            = "C:\Db\$($sqlServerVersionData.Version)\Backup"
+            UpdateEnabled                           = 'False'
+            SuppressReboot                          = $true # Make sure we don't reboot during testing.
+            ForceReboot                             = $false
+
+            # Properties for downloading media
+            DownloadExePath                         = Join-Path -Path $env:TEMP -ChildPath $sqlServerVersionData.DownloadExeName
+            DownloadIsoPath                         = Join-Path -Path $env:TEMP -ChildPath $sqlServerVersionData.DownloadIsoName
+
+            # Properties for mounting media
+            ImagePath                               = Join-Path -Path $env:TEMP -ChildPath $sqlServerVersionData.IsoImageName
+            DriveLetter                             = $mockIsoMediaDriveLetter
+
+            # Parameters to configure TempDb
+            SqlTempDbFileCount                      = '2'
+            SqlTempDbFileSize                       = '128'
+            SqlTempDbFileGrowth                     = '128'
+            SqlTempDbLogFileSize                    = '128'
+            SqlTempDbLogFileGrowth                  = '128'
+
+            SqlInstallAccountUserName               = "$env:COMPUTERNAME\SqlInstall"
+            SqlInstallAccountPassword               = 'P@ssw0rd1'
+            SqlAdministratorAccountUserName         = "$env:COMPUTERNAME\SqlAdmin"
+            SqlAdministratorAccountPassword         = 'P@ssw0rd1'
+            SqlServicePrimaryAccountUserName        = "$env:COMPUTERNAME\svc-SqlPrimary"
+            SqlServicePrimaryAccountPassword        = 'yig-C^Equ3'
+            SqlAgentServicePrimaryAccountUserName   = "$env:COMPUTERNAME\svc-SqlAgentPri"
+            SqlAgentServicePrimaryAccountPassword   = 'yig-C^Equ3'
+            #SqlServiceSecondaryAccountUserName      = "$env:COMPUTERNAME\svc-SqlSecondary"
+            #SqlServiceSecondaryAccountPassword      = 'yig-C^Equ3'
+            #SqlAgentServiceSecondaryAccountUserName = "$env:COMPUTERNAME\svc-SqlAgentSec"
+            #SqlAgentServiceSecondaryAccountPassword = 'yig-C^Equ3'
+
+            #CertificateFile                         = $env:DscPublicCertificatePath
+        }
+    )
+}
+
+
+<#
+    Creating all the credential objects to save some repeating code.
+#>
+
+$SqlInstallCredential = New-Object `
+    -TypeName System.Management.Automation.PSCredential `
+    -ArgumentList @($ConfigurationData.AllNodes.SqlInstallAccountUserName,
+        (ConvertTo-SecureString -String $ConfigurationData.AllNodes.SqlInstallAccountPassword -AsPlainText -Force))
+
+$SqlAdministratorCredential = New-Object `
+    -TypeName System.Management.Automation.PSCredential `
+    -ArgumentList @($ConfigurationData.AllNodes.SqlAdministratorAccountUserName,
+        (ConvertTo-SecureString -String $ConfigurationData.AllNodes.SqlAdministratorAccountPassword -AsPlainText -Force))
+
+$SqlServicePrimaryCredential = New-Object `
+    -TypeName System.Management.Automation.PSCredential `
+    -ArgumentList @($ConfigurationData.AllNodes.SqlServicePrimaryAccountUserName,
+        (ConvertTo-SecureString -String $ConfigurationData.AllNodes.SqlServicePrimaryAccountPassword -AsPlainText -Force))
+
+$SqlAgentServicePrimaryCredential = New-Object `
+    -TypeName System.Management.Automation.PSCredential `
+    -ArgumentList @($ConfigurationData.AllNodes.SqlAgentServicePrimaryAccountUserName,
+        (ConvertTo-SecureString -String $ConfigurationData.AllNodes.SqlAgentServicePrimaryAccountPassword -AsPlainText -Force))
+
+# $SqlServiceSecondaryCredential = New-Object `
+#     -TypeName System.Management.Automation.PSCredential `
+#     -ArgumentList @(
+#         $ConfigurationData.AllNodes.SqlServiceSecondaryAccountUserName,
+#                 (ConvertTo-SecureString -String $ConfigurationData.AllNodes.SqlServiceSecondaryAccountPassword -AsPlainText -Force))
+
+# $SqlAgentServiceSecondaryCredential = New-Object `
+#     -TypeName System.Management.Automation.PSCredential `
+#     -ArgumentList @($ConfigurationData.AllNodes.SqlAgentServiceSecondaryAccountUserName,
+#         (ConvertTo-SecureString -String $ConfigurationData.AllNodes.SqlAgentServiceSecondaryAccountPassword -AsPlainText -Force))
+
+
+<#
+    .SYNOPSIS
+        Setting up the dependencies to test installing SQL Server instances.
+#>
+Configuration DSC_SqlSetup_CreateDependencies_Config
+{
+    Import-DscResource -ModuleName 'PSDscResources' -ModuleVersion '2.12.0.0'
+    Import-DscResource -ModuleName 'StorageDsc' -ModuleVersion '4.9.0.0'
+
+    node $AllNodes.NodeName
+    {
+        MountImage 'MountIsoMedia'
+        {
+            ImagePath   = $Node.ImagePath
+            DriveLetter = $Node.DriveLetter
+            Ensure      = 'Present'
+        }
+
+        WaitForVolume WaitForMountOfIsoMedia
+        {
+            DriveLetter      = $Node.DriveLetter
+            RetryIntervalSec = 5
+            RetryCount       = 10
+        }
+
+        User 'CreateSqlServicePrimaryAccount'
+        {
+            Ensure   = 'Present'
+            UserName = Split-Path -Path $SqlServicePrimaryCredential.UserName -Leaf
+            Password = $SqlServicePrimaryCredential
+        }
+
+        User 'CreateSqlAgentServicePrimaryAccount'
+        {
+            Ensure   = 'Present'
+            UserName = Split-Path -Path $SqlAgentServicePrimaryCredential.UserName -Leaf
+            Password = $SqlAgentServicePrimaryCredential
+        }
+
+        # User 'CreateSqlServiceSecondaryAccount'
+        # {
+        #     Ensure   = 'Present'
+        #     UserName = Split-Path -Path $SqlServiceSecondaryCredential.UserName -Leaf
+        #     Password = $SqlServicePrimaryCredential
+        # }
+
+        # User 'CreateSqlAgentServiceSecondaryAccount'
+        # {
+        #     Ensure   = 'Present'
+        #     UserName = Split-Path -Path $SqlAgentServiceSecondaryCredential.UserName -Leaf
+        #     Password = $SqlAgentServicePrimaryCredential
+        # }
+
+        User 'CreateSqlInstallAccount'
+        {
+            Ensure   = 'Present'
+            UserName = Split-Path -Path $SqlInstallCredential.UserName -Leaf
+            Password = $SqlInstallCredential
+        }
+
+        Group 'AddSqlInstallAsAdministrator'
+        {
+            Ensure           = 'Present'
+            GroupName        = 'Administrators'
+            MembersToInclude = Split-Path -Path $SqlInstallCredential.UserName -Leaf
+        }
+
+        User 'CreateSqlAdminAccount'
+        {
+            Ensure   = 'Present'
+            UserName = Split-Path -Path $SqlAdministratorCredential.UserName -Leaf
+            Password = $SqlAdministratorCredential
+        }
+
+        WindowsFeature 'NetFramework45'
+        {
+            Name   = 'NET-Framework-45-Core'
+            Ensure = 'Present'
+        }
+    }
+}
