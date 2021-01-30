327af6090642ca15c8bd3e32072c6463c2d6855c
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2021-01-30 14:05:19 +0000

    Added initial script for 'AzureDevOps' installation from EXE within integration tests.

--- tests/Integration/Setup/AzureDevOpsDsc.AzureDevOps.Integration.Tests.ps1
+++ tests/Integration/Setup/AzureDevOpsDsc.AzureDevOps.Integration.Tests.ps1
@@ -20,6 +20,41 @@ catch [System.IO.FileNotFoundException]
 }
 
 
+<#
+    .SYNOPSIS
+        This function will output the Azure DevOps log files.
+    .DESCRIPTION
+        This function will output the Azure DevOps log files, this is to be
+        able to debug any problems that potentially occurred during setup.
+#>
+function Show-AzureDevOpsLog
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [System.String]
+        $Path
+    )
+
+    $summaryLogPath = Get-ChildItem -Path $$Path |
+        Sort-Object -Property LastWriteTime -Descending |
+        Select-Object -First 1
+
+    $summaryLog = Get-Content $summaryLogPath
+
+    Write-Verbose -Message $('-' * 80) -Verbose
+    Write-Verbose -Message $(Split-Path -Path $summaryLogPath -Leaf) -Verbose
+    Write-Verbose -Message $('-' * 80) -Verbose
+
+    $summaryLog | ForEach-Object -Process {
+        Write-Verbose $_ -Verbose
+    }
+
+    Write-Verbose -Message $('-' * 80) -Verbose
+}
+
+
 # Dot-source configuration to be used in subsquent tests/executions
 $configFile = Join-Path -Path $PSScriptRoot -ChildPath "AzureDevOpsDsc.AzureDevOps.config.ps1"
 . $configFile
@@ -84,3 +119,32 @@ else
 {
     Write-Verbose -Message 'Azure DevOps Server media (EXE) is already present/downloaded.' -Verbose
 }
+
+
+# Install Azure DevOps using the EXE
+$previousLocation = Get-Location
+
+Write-Verbose -Message 'Installing Azure DevOps Server...' -Verbose
+Set-Location -Path $(Split-Path -Path $ConfigurationData.AllNodes.DownloadExePath -Parent)
+$expression = '{0} /CustomInstallPath {1}" /Full /NoRefresh /NoRestart /NoWeb /Passive /ProductKey TRIAL /Silent /Log {2}' -f $azureDevOpsServerVersionData.ExeName, $ConfigurationData.AllNodes.InstallPath, $ConfigurationData.AllNodes.LogFilePath
+Write-Verbose -Message $('Executing: "{0}"' -f $expression) -Verbose
+& $expression
+Write-Verbose -Message 'Successfully installed Azure DevOps Server.' -Verbose
+
+Write-Verbose -Message 'Outputting AzureDevOps installation logs...' -Verbose
+Show-AzureDevOpsLog -Path $($azureDevOpsServerVersionData.LogsDirectoryPath + '\*.txt')
+
+
+
+Write-Verbose -Message 'Configuring Azure DevOps Server...' -Verbose
+Set-Location -Path $(Join-Path -Path $ConfigurationData.AllNodes.InstallPath -ChildPath 'Tools')
+$expression = 'TODO: See "https://docs.microsoft.com/en-us/azure/devops/server/command-line/tfsconfig-cmd?view=azure-devops-2019#unattend"' # TODO:
+Write-Verbose -Message $('Executing: "{0}" ...' -f $expression) -Verbose
+#& $expression
+Write-Verbose -Message 'Successfully configured Azure DevOps Server.' -Verbose
+
+Write-Verbose -Message 'Outputting AzureDevOps configuration logs...' -Verbose
+Show-AzureDevOpsLog -Path 'C:\ProgramData\Microsoft\Azure DevOps\Server Configuration\Logs\*.log'
+
+Set-Location -Path $previousLocation
+
